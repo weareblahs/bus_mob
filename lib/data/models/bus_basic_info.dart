9d63dd4256536f6ebedc7f4c9de40668f95913a3
Tan Yu Xuan <tanyuxuan2005@gmail.com>
2025-05-20 22:19:30 +0800

    add previous, current, next model to BusStations class that will be used later on

--- lib/data/models/bus_basic_info.dart
+++ lib/data/models/bus_basic_info.dart
@@ -4,6 +4,7 @@ class BusBasicInfo {
   double? latitude;
   double? longitude;
   String? currentParsedLocation = "Unknown location";
+  BusStations? busStations = BusStations();
 
   BusBasicInfo({
     required this.licensePlate,
@@ -11,10 +12,32 @@ class BusBasicInfo {
     required this.latitude,
     required this.longitude,
     this.currentParsedLocation,
+    this.busStations,
   });
 
   @override
   String toString() {
-    return "BusBasicInfo: $licensePlate, $speed, $latitude, $longitude, $currentParsedLocation";
+    return "BusBasicInfo: $licensePlate, $speed, $latitude, $longitude, $currentParsedLocation, $busStations";
+  }
+}
+
+class BusStations {
+  String? previousStation = "Unknown Station";
+  String? currentStation = "Unknown Station";
+  String? nextStation = "Unknown Station";
+  double? distanceToCurrent = -1;
+  double? timeRemaining = -1;
+
+  BusStations({
+    this.previousStation,
+    this.currentStation,
+    this.nextStation,
+    this.distanceToCurrent,
+    this.timeRemaining,
+  });
+
+  @override
+  String toString() {
+    return "BusStations($previousStation, $currentStation, $nextStation, $distanceToCurrent, $timeRemaining)";
   }
 }
