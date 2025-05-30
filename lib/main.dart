f763e4e05fb0ebc9579c8e8cb0808474d5f8a84f
Tan Yu Xuan <tanyuxuan2005@gmail.com>
2025-05-30 19:03:35 +0800

    remove unused packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bus_mob/data/navigation/navigation.dart';
+import 'data/navigation/navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
